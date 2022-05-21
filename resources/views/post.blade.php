cd6446b031ec6bd535f746f14ef494c608ea2b1c
Rafi Rahman <rafirahmann18@gmail.com>
2022-05-21 10:01:53 +0700

    eps 7

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.main')
 @section('container')
 <article>
-    <h2>{{ $post['title'] }}</h2>
-    <h5>By: {{ $post['author'] }}</h5>
-    <p>{{ $post['content'] }}</p>
+    <h2>{{ $post->title }}</h2>
+    <h5>By: {{ $post->author }}</h5>
+    {!! $post->body !!}
 </article>
 <a href="/posts">Back to posts list</a>
 @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,9 +2,9 @@
 @section('container')
 @foreach ($posts as $post)
     <article class="mb-5">
-        <h2><a href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a></h2>
-        <h5>By: {{ $post['author'] }}</h5>
-        <p>{{ $post['content'] }}</p>
+        <h2><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
+        <h5>By: {{ $post->author }}</h5>
+        <p>{{ $post->excerpt }}</p>
     </article>
 @endforeach
 @endsection
