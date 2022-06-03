77d322ca6615ed6e4fe18a95a77279fb37d608aa
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-03 18:12:23 +0700

    eps 8

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,8 @@
 @section('container')
 <article>
     <h2>{{ $post->title }}</h2>
-    <h5>By: {{ $post->author }}</h5>
+    <h5>Category: {{ $post->category->name }}</h5>
+    {{-- <h5>By: {{ $post->author }}</h5> --}}
     {!! $post->body !!}
 </article>
 <a href="/posts">Back to posts list</a>
