cbab8c6f032e9041c32718dee6c71bbd00cbcb5d
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-04 08:38:11 +0700

    eps 9

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,7 @@
 @section('container')
 <article>
     <h2>{{ $post->title }}</h2>
-    <h5>Category: <a href="/category/{{ $post->category->slug }}">{{ $post->category->name }}</a></h5>
+    <p>By. Rafi Rahman in <a href="/category/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
     {{-- <h5>By: {{ $post->author }}</h5> --}}
     {!! $post->body !!}
 </article>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.main')
 @section('container')
+<h1>Posts Page</h1>
 @foreach ($posts as $post)
     <article class="mb-5">
         <h2><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
+        <p>By. Rafi Rahman in <a href="/category/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         <p>{{ $post->excerpt }}</p>
     </article>
 @endforeach
