eae1b69d868d94b4cbd66bc7e1777af5fae54298
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-04 08:46:12 +0700

    eps 9

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('container')
-<article>
-    <h2>{{ $post->title }}</h2>
+<article class="mb-5">
+    <h2 class="mb-5">{{ $post->title }}</h2>
     <p>By. Rafi Rahman in <a href="/category/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
     {{-- <h5>By: {{ $post->author }}</h5> --}}
     {!! $post->body !!}
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.main')
 @section('container')
-<h1>Posts Page</h1>
+<h1 class="mb-5">Posts Page</h1>
 @foreach ($posts as $post)
-    <article class="mb-5">
-        <h2><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
-        <p>By. Rafi Rahman in <a href="/category/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <article class="mb-5 border-bottom pb-4">
+        <h3><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h3>
+        <p>By. Rafi Rahman in <a href="/category/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
         <p>{{ $post->excerpt }}</p>
+        <a href="/post/{{ $post->slug }}">Read more...</a>
     </article>
 @endforeach
 @endsection
