f5e9b659721ddf5ee223b2bd56e3ce1f8cb4f7d3
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-06 15:47:29 +0700

    eps 10

--- resources/views/author.blade.php
+++ resources/views/author.blade.php
@@ -4,9 +4,10 @@
     @foreach ($posts as $post)
     <article class="mb-5 border-bottom pb-4">
         <h2 class="">{{ $post->title }}</h2>
-        <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/category/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+        <p>By. <a href="/author/{{ $post->user->slug }}" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/category/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
         {{-- <h5>By: {{ $post->author }}</h5> --}}
-        {!! $post->body !!}
+        <p>{!! $post->excerpt !!}</p>
+        <p><a href="/post/{{ $post->slug }}" class="text-decoration-none">Read more...</a></p>
     </article>
     @endforeach
 @endsection
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,7 @@
 @section('container')
 <article class="mb-5 border-bottom pb-4">
     <h2 class="mb-5">{{ $post->title }}</h2>
-    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/category/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+    <p>By. <a href="/author/{{ $post->user->slug }}" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/category/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
     {{-- <h5>By: {{ $post->author }}</h5> --}}
     {!! $post->body !!}
 </article>
