5cffc6b1f0a3dae3496d1b3a7a17bfa63d7785d2
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-07 13:28:19 +0700

    eps 12

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,21 +1,30 @@
 @extends('layouts.main')
 @section('container')
+    <h1 class="mb-5">{{ $title }}</h1>
     @if ($posts->count())
         <div class="card mb-3">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                    content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <img src="https://source.unsplash.com/random/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top"
+                alt="...">
+            <div class="card-body text-center">
+                <h5 class="card-title text-decoration-none text-dark"><a href="/post/{{ $posts[0]->slug }}"
+                        class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h5>
+                <p>
+                    <small class="text-muted">
+                        By. <a href="/authors/{{ $posts[0]->author->username }}"
+                            class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a
+                            href="/category/{{ $posts[0]->category->slug }}"
+                            class="text-decoration-none">{{ $posts[0]->category->name }}</a>
+                        {{ $posts[0]->created_at->diffForHumans() }}
+                    </small>
+                </p>
+                <p class="card-text">{{ $posts[0]->excerpt }}</p>
+                <a href="/post/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more</a>
             </div>
         </div>
     @else
         <p class="text-center fs-4">Post not found.</p>
     @endif
-    <h1 class="mb-5">{{ $title }}</h1>
-
-    @foreach ($posts as $post)
+    @foreach ($posts->skip(1) as $post)
         <article class="mb-5 border-bottom pb-4">
             <h3><a href="/post/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h3>
             <p>By. <a href="/authors/{{ $post->author->username }}"
