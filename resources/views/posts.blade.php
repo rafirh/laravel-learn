9db2b1150871b5b774fe0e795fbf894f1fa55041
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-07 13:43:20 +0700

    eps 12

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,19 +4,11 @@
     @if ($posts->count())
         <div class="card mb-3">
             <img src="https://source.unsplash.com/random/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top"
-                alt="...">
+                alt="{{ $posts[0]->category->name }}">
             <div class="card-body text-center">
                 <h5 class="card-title text-decoration-none text-dark"><a href="/post/{{ $posts[0]->slug }}"
                         class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h5>
-                <p>
-                    <small class="text-muted">
-                        By. <a href="/authors/{{ $posts[0]->author->username }}"
-                            class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a
-                            href="/category/{{ $posts[0]->category->slug }}"
-                            class="text-decoration-none">{{ $posts[0]->category->name }}</a>
-                        {{ $posts[0]->created_at->diffForHumans() }}
-                    </small>
-                </p>
+
                 <p class="card-text">{{ $posts[0]->excerpt }}</p>
                 <a href="/post/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more</a>
             </div>
@@ -24,15 +16,37 @@
     @else
         <p class="text-center fs-4">Post not found.</p>
     @endif
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="https://source.unsplash.com/random/1200x400/?{{ $post->category->name }}"
+                        class="card-img-top" alt="{{ $post->category->name }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $post->title }}</h5>
+                        <p>
+                            <small class="text-muted">
+                                By. <a href="/authors/{{ $post->author->username }}"
+                                    class="text-decoration-none">{{ $post->author->name }}</a>
+                                {{ $post->created_at->diffForHumans() }}
+                            </small>
+                        </p>
+                        <p class="card-text">{{ $post->excerpt }}</p>
+                        <a href="/post/{{ $post->slug }}" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     @foreach ($posts->skip(1) as $post)
         <article class="mb-5 border-bottom pb-4">
-            <h3><a href="/post/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h3>
+            <h3><a href="/post/{{ $post->slug }}" class="text-decoration-none"></a></h3>
             <p>By. <a href="/authors/{{ $post->author->username }}"
                     class="text-decoration-none">{{ $post->author->name }}</a> in <a
                     href="/category/{{ $post->category->slug }}"
                     class="text-decoration-none">{{ $post->category->name }}</a></p>
-            <p>{{ $post->excerpt }}</p>
-            <a href="/post/{{ $post->slug }}" class="text-decoration-none">Read more...</a>
+            <p></p>
+            <a href="" class="text-decoration-none">Read more...</a>
         </article>
     @endforeach
 @endsection
