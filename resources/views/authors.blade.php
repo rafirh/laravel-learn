7936d976bbbfedac7c7a79d9fb8049301b7f4c7c
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-08 09:25:33 +0700

    eps 12

--- resources/views/authors.blade.php
+++ resources/views/authors.blade.php
@@ -1,9 +1,28 @@
 @extends('layouts.main')
 @section('container')
-<h1 class="mb-5">Post Category:</h1>
-@foreach ($authors as $author)
-    <article class="mb-5">
-        <h3><a href="/authors/{{ $author->slug }}" class="text-decoration-none">{{ $author->name }}</a></h3>
-    </article>
-@endforeach
+    <h1 class="mb-5">Post Category:</h1>
+    <div class="conatainer">
+        <div class="row">
+            @foreach ($authors as $author)
+                <div class="col-md-4 mb-5">
+                    <a href="/authors/{{ $author->username }}">
+                        <div class="card bg-dark text-white border-0">
+                            <img src="https://source.unsplash.com/random/400x300?person"
+                                class="card-img" alt="{{ $author->name }}">
+                            <div class="card-img-overlay d-flex align-items-center p-0">
+                                <h5 class="card-title text-center flex-fill p-4 fs-3"
+                                    style="background-color: rgba(0, 0, 0, 0.7)">{{ $author->name }}</h5>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+    {{-- @foreach ($authors as $author)
+        <article class="mb-5">
+            <h3><a href="/authors/{{ $author->slug }}" class="text-decoration-none">{{ $author->name }}</a></h3>
+        </article>
+    @endforeach --}}
 @endsection
