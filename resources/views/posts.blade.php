b6e45f67c7bebbe3790f0469c31156ce1bfa6d33
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-10 10:08:13 +0700

    eps 13

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.main')
 @section('container')
-    @if ($posts->count())
-        <h1 class="mb-3 text-center">{{ $title }}</h1>
-        <div class="row justify-content-center mb-3">
-            <div class="col-md-6">
-                <form action="/posts" method="GET">
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Search..." name="search" value="{{ request('search') }}">
-                        <button class="btn btn-danger" type="submit">Search</button>
-                    </div>
-                </form>
-            </div>
+
+    <h1 class="mb-3 text-center">{{ $title }}</h1>
+    <div class="row justify-content-center mb-3">
+        <div class="col-md-6">
+            <form action="/posts" method="GET">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Search..." name="search"
+                        value="{{ request('search') }}">
+                    <button class="btn btn-danger" type="submit">Search</button>
+                </div>
+            </form>
         </div>
+    </div>
+    @if ($posts->count())
         <div class="card mb-3">
             <div class="position-absolute px-3 py-2" style="background-color: rgba(0, 0, 0, 0.7)"><a
                     href="/categories/{{ $posts[0]->category->slug }}"
