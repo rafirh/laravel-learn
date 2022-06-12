391bede7e828d5d3322208a5d8eb6b8b10cdfa84
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-12 07:58:53 +0700

    eps 14

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -27,7 +27,7 @@
         </ul>
         <ul class="navbar-nav ms-auto">
           <li class="nav-item">
-            <a href="" class="nav-link {{ (request()->segment(1) == 'login' ? 'active') : '' }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+            <a href="/login" class="nav-link {{ (request()->segment(1) == 'login') ? 'active' : '' }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
           </li>
         </ul>
       </div>
