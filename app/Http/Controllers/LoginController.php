c7a4ab2da8a39c08c77ec10d2cfc56281c9b1a50
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-13 14:13:10 +0700

    eps 16 done

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -29,5 +29,7 @@ class LoginController extends Controller
     public function logout(Request $request){
         Auth::logout();
         $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
     }
 }
