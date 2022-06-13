d0d54a69af8711d0d4bd7477e9bf94d64af2a77a
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-13 10:56:55 +0700

    eps 15 done

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row justify-content-center">
         <div class="col-md-4">
             @if (session('success'))
-                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
                     {{ session('success') }}
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
