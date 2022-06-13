e9ad8a744070cc784c9cf9d75c1dd30e4681b24d
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-13 10:16:13 +0700

    eps 15

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -11,12 +12,13 @@ class RegisterController extends Controller
             'title' => 'Register'
         ]);
     }
-    public function store(Request $request){
-        $request->validate([
+    public function store(Request $request, User $user){
+        $validatedData = $request->validate([
             'name' => 'required|max:255',
             'username' => 'required|min:3|max:255|unique:users',
             'email' => 'required|email:dns|unique:users|max:255',
             'password' => 'required|min:6|max:255'
         ]);
+        $user->create($validatedData);
     }
 }
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -7,7 +7,7 @@
                 <form action="/register" method="POST">
                     @csrf
                     <div class="form-floating mb-1">
-                        <input type="text" class="form-control @error('name') {{ "is-invalid" }} @enderror" id="name" name="name" placeholder="Name">
+                        <input type="text" class="form-control @error('name') {{ "is-invalid" }} @enderror" id="name" name="name" placeholder="Name" value="{{ old('name') }}" required>
                         <label for="name">Full Name</label>
                         @error('name')
                             <div class="invalid-feedback">
@@ -16,7 +16,7 @@
                         @enderror
                     </div>
                     <div class="form-floating mb-1">
-                        <input type="text" class="form-control @error('username') {{ "is-invalid" }} @enderror" id="username" name="username" placeholder="Username">
+                        <input type="text" class="form-control @error('username') {{ "is-invalid" }} @enderror" id="username" name="username" placeholder="Username" value="{{ old('username') }}" required>
                         <label for="username">Username</label>
                         @error('username')
                             <div class="invalid-feedback">
@@ -25,7 +25,7 @@
                         @enderror    
                     </div>
                     <div class="form-floating mb-1">
-                        <input type="email" class="form-control @error('email') {{ "is-invalid" }} @enderror" id="email" name="email" placeholder="name@example.com">
+                        <input type="email" class="form-control @error('email') {{ "is-invalid" }} @enderror" id="email" name="email" placeholder="name@example.com" value="{{ old('email') }}" required>
                         <label for="email">Email Address</label>
                         @error('email')
                             <div class="invalid-feedback">
@@ -34,7 +34,7 @@
                         @enderror
                     </div>
                     <div class="form-floating mb-1">
-                        <input type="password" class="form-control @error('password') {{ "is-invalid" }} @enderror" id="password" name="password" placeholder="Password">
+                        <input type="password" class="form-control @error('password') {{ "is-invalid" }} @enderror" id="password" name="password" placeholder="Password" value="{{ old('password') }}" required>
                         <label for="password">Password</label>
                         @error('password')
                             <div class="invalid-feedback">
