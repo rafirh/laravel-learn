0d7688add27a9cc81936be200bbea6d3c00972da
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-15 14:06:32 +0700

    eps 20

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -10,11 +10,6 @@ use Illuminate\Support\Str;
 
 class DashboardPostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return view('dashboard.posts.index', [
@@ -22,12 +17,6 @@ class DashboardPostController extends Controller
             'posts' => Post::where('user_id', auth()->user()->id)->get()
         ]);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('dashboard.posts.create', [
@@ -35,13 +24,6 @@ class DashboardPostController extends Controller
             'categories' => Category::all()
         ]);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $validatedData = $request->validate([
@@ -57,13 +39,6 @@ class DashboardPostController extends Controller
         Post::create($validatedData);
         return redirect('/dashboard/posts')->with('success', 'A new post has been created!');
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
     public function show(Post $post)
     {
         return view('dashboard.posts.show', [
@@ -71,13 +46,6 @@ class DashboardPostController extends Controller
             'post' => $post
         ]);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Post $post)
     {
         return view('dashboard.posts.edit', [
@@ -86,31 +54,28 @@ class DashboardPostController extends Controller
             'categories' => Category::all()
         ]);
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Post $post)
     {
-        //
-    }
+        $rules = [
+            'title' => 'required|max:255',
+            'category_id' => 'required',
+            'body' => 'required'
+        ];
+        if($request->slug != $post->slug){
+            $rules['slug'] = 'required|unique:posts';
+        }
+        $validatedData = $request->validate($rules);
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
+        Post::where('id', $post->id)->update($validatedData);
+        return redirect('/dashboard/posts')->with('success', 'Post has been edited!');
+    }
     public function destroy(Post $post)
     {
         Post::destroy($post->id);
         return redirect('/dashboard/posts')->with('success', 'Post has been deleted!');
     }
-
     public function createSlug(Request $request)
     {
         $slug = SlugService::createSlug(Post::class, 'slug', $request->title);
