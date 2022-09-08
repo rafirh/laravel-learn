5b30c3533660ad9178d9cbf68a801c33d527d0a7
Rafi Rahman <rafirahmann18@gmail.com>
2022-09-08 15:45:33 +0700

    first commit

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
         <div class="row justify-content-center mb-5">
             <div class="col-md-8">
                 <h2 class="mb-3">{{ $post->title }}</h2>
-                <p>By. <a href="/posts?author={{ $post->author->slug }}"
+                <p>By. <a href="/posts?author={{ $post->author->username }}"
                         class="text-decoration-none">{{ $post->author->name }}</a> in <a
                         href="/posts?category{{ $post->category->slug }}"
                         class="text-decoration-none">{{ $post->category->name }}</a></p>
