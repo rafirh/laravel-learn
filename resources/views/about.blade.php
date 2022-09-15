5f4e68e9ebf51f7f7fd4c3095741df3ca18eec6a
Rafi Rahman <rafirahmann18@gmail.com>
2022-09-15 12:05:33 +0700

    first commit in branch kembali

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,4 @@
 @extends('layouts.main')
 @section('container')
-    <h1>This is about page.</h1>
-    <h3>{{ $admin['name'] }}</h3>
-    <p>{{ $admin['email'] }}</p>
-    <img src="img/{{ $admin['photo'] }}" alt="{{ $admin['photo'] }}" width="200" class="img-thumbnail rounded-circle">    
+    <h1>About page</h1>
 @endsection
